bec4d91825f8360387ff58d884780994320d2fa6
Jenly <jenly1314@gmail.com>
2026-09-18 23:56:39 +0800

    优化细节

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -19,9 +19,9 @@ pluginManagement {
     }
 
     repositories {
-        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { url = uri("https://maven.aliyun.com/repository/central") }
-        maven { url = uri("https://maven.aliyun.com/repository/google") }
+//        maven { url = uri("https://maven.aliyun.com/repository/gradle-plugin") }
+//        maven { url = uri("https://maven.aliyun.com/repository/central") }
+//        maven { url = uri("https://maven.aliyun.com/repository/google") }
         gradlePluginPortal()
         google()
         mavenCentral()
